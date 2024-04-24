6d1490225f97b51652e55ed77bfbc7e0aab5780b
Ferdyan Darwis <ferdyan.darwis.fd@gmail.com>
2024-04-24 20:13:33 +0700

    Revert "Tugas Migration, Seeder, dan Factory"
    This reverts commit 15462ff4149f77c3431ca496c724f586e70679b4.

--- database/factories/StaffFactory.php
+++ database/factories/StaffFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 use App\Models\Staff;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+//
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Staff>
  */
